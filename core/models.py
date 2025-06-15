877334d183ef995e0e6c24f34a40d1d537cc56d0
AlishxAdhikari <alishadhikari42@gmail.com>
2025-06-15 13:40:27 +0545

    Django project LMS: Student & Teacher model added and registered in admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Student(models.Model):
+    SEMESTER = [
+        ('SEM_ONE', 'Semester One'),
+        ('SEM_TWO', 'Semester Two'),
+        ('SEM_THREE', 'Semester Three'),
+        ('SEM_FOUR', 'Semester Four'),
+        ('SEM_FIVE', 'Semester Five'),
+        ('SEM_SIX', 'Semester Six'),
+        ('SEM_SEVEN', 'Semester Seven'),
+        ('SEM_EIGHT', 'Semester Eight'),
+    ]
+    full_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Student Full Name")
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="Student Email")
+    semester = models.Field(choices=SEMESTER, default='N/A', null=True, blank=True)
+    phone_no = models.IntegerField(null=False,blank=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    class meta:
+        verbose_name = "student"
+        verbose_name_plural = "students"
+        ordering =['-full_name']
+
+
+
+        def __str__(self):
+            return self.full_name
+        
+class Teacher(models.Model):
+    DEPARTMENT = {
+        ('BCA','Bachelor of Computer Application'),
+        ('BCT','Bachelor of Computer Engineering'),
+        ('BEI','Bachelor of Electronics and Information'),
+        ('BCE','Bachelor of Civil Engineering'),
+    }
+    full_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="teacher Full Name")
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="teacher Email")
+    department = models.Field(choices=DEPARTMENT, default='N/A', null=True, blank=True)
+    phone_no = models.IntegerField(null=False,blank=False)
+    join_data = models.DateField(default='Join Data')
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    class meta:
+        verbose_name = "teacher"
+        verbose_name_plural = "teachers"
+        ordering =['-full_name']
+
+
+
+        def __str__(self):
+            return self.full_name
+            
